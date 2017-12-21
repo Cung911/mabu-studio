f26fa4c906a707845ff054066cdabbd12b29809c
Cung911 <Cung911@gmail.com>
2017-12-21 23:54:34 +0700

    Ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,53 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var jade        = require('gulp-jade');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['src/scss/**/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/assets/css"))
         .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("dist/assets/js"))
         .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
-
     browserSync.init({
-        server: "./src"
+        server: "./dist",
+        open: false
     });
-
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['src/scss/**/*.scss'], ['sass']);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
+    gulp.watch('src/templates/**/*.jade', ['jade'])
 });
 
 // Move Fonts to src/fonts
 gulp.task('fonts', function() {
   return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
+    .pipe(gulp.dest('dist/assets/fonts'))
 })
 
 // Move Font Awesome CSS to src/css
 gulp.task('fa', function() {
   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('dist/assets/css/vendors'))
 })
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+// Jade Template
+gulp.task('jade', function(){
+  gulp.src('src/templates/*.jade')
+  .pipe(jade({
+    pretty:true
+  }))
+  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['js','serve', 'fa', 'fonts', 'jade']);
